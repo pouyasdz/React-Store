734bbac8e1de1c709e77b576baa71f903863d980
pouyasdz <qpdevil83@gmail.com>
2022-12-31 18:49:49 +0330

    config router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import Container from "./container";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./page/home";
+import Product from "./page/product";
+import CategoryOverview from "./page/category/CategoryOverview";
+import CategoryProducts from "./page/category/CategoryProducts";
+import Cart from "./page/cart";
 
 function App() {
   return (
-    <div className="app">
-      <Container>
-        <div style={{height:"100vh"}}>Child Component</div>
-      </Container>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Container children={<Home/>}/> }/>
+        
+        <Route path="category">
+          <Route path="all" element={ <Container children={<CategoryOverview/>}/> }/>
+          <Route path=":id" element={ <Container children={<CategoryProducts/>}/> }/>
+        </Route>
+
+        <Route path="product">
+          <Route path=":id" element={ <Container children={<Product/>}/> }/>
+        </Route>
+
+        <Route path="cart">
+          <Route path=":id" element={ <Container children={<Cart/>}/> }/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/page/category/CategoryOverview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CategoryOverview() {
+  return (
+    <div>CategoryOverview</div>
+  )
+}
+
+export default CategoryOverview
