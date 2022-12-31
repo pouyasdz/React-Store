c4fac2f75bdabbcf072d913a809463bd25adf10c
pouyasdz <qpdevil83@gmail.com>
2022-12-31 11:33:05 +0330

    fix responsiv footer

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Footer from '../components/common/Footer';
 import NavBar from '../components/common/NavBar';
 import children from '../Types/common/children'
-
+import "../styles/layout.css"
 function Container(props:children) {
   return (
     <div className='container mx-auto block' style={{height:"100vh", maxHeight:"100vh", overflow:"hidden"}}>
         <NavBar/>
      
-        <div className="container bg-blue-500 overflow-auto" style={{height:"96vh", maxHeight:"96vh"}}>
+        <div className="container bg-blue-500 overflow-auto content">
           {props.children}
         <Footer/>
         </div>
